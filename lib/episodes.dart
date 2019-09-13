b31e1cf73b65edb425dc1243eba373fa528a6f6c
bmd007 <bmd579@gmail.com>
2019-09-13 08:33:57 +0200

    initail|

---
+++ lib/episodes.dart
@@ -0,0 +1,27 @@
+import 'package:meta/meta.dart';
+
+class EpisodePlaylist {
+
+  List<Episode> episodes;
+
+  EpisodePlaylist({
+    @required this.episodes,
+  });
+
+}
+
+class Episode {
+
+  final String audioUrl;
+  final String albumArtUrl;
+  final String title;
+  final String artist;
+
+  Episode({
+    @required this.audioUrl,
+    @required this.albumArtUrl,
+    @required this.title,
+    @required this.artist,
+  });
+
+}
